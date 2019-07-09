20ccd7bd18c553b43ae901b0dd0a466197fed451
ssp19-026 <ssp19-026@id.nmt.edu>
2019-07-09 00:33:29 -0600

    Added Astronmetry
    Added astrometry code and supplemental code

---
+++ ODCode/Astrometry.py
@@ -0,0 +1,79 @@
+import math as m
+import numpy as np
+from astropy.io import fits
+
+#commented out code was code used testing code on Jaiden's values
+
+from CircularCentroid import centroid
+from LSPR import LSPR2
+def RAtoDegree(sRA):
+    sDeg = 0
+    arr = sRA.split(":")
+    for i in range(len(arr)):
+        if i == 0:
+            sDeg = sDeg + float(arr[i]) * 15
+        else:
+            sDeg = sDeg + 360 * float(arr[i])/(24 * 60 ** i)
+    return sDeg
+def DtoDegree(sD):
+    sDeg = 0
+    arr = sD.split(":")
+    for i in range(len(arr)):
+        if i == 0:
+            sDeg = sDeg + float(arr[i]) 
+        else:
+            sDeg = sDeg + float(arr[i])/(60**i)
+    return sDeg
+
+#defining constants
+radius1 = 10
+radius2 = 3
+astX = 622
+astY = 403
+arrRA = []
+arrDEC = []
+arrX = []
+arrY = []
+arrcentroidX = []
+arrcentroidY = []
+
+
+im = fits.getdata('Aligned_reduced0000.fits')
+#im = fits.getdata('jaiden.fits')
+image = np.array(im)
+
+#parsing raw input into respective lists
+for line in open('row.txt.txt','r'):
+    arrline = line.split(" ")
+    arrRA.append(RAtoDegree(arrline[0]))
+    arrDEC.append(DtoDegree(arrline[1]))
+    arrX.append(arrline[2])
+    arrY.append(arrline[3])
+
+
+##for line in open('row2.txt','r'):
+##    arrline = line.split(" ")
+##    arrX.append(arrline[0])
+##    arrY.append(arrline[1])
+##    arrRA.append(RAtoDegree(arrline[2]))
+##    arrDEC.append(DtoDegree(arrline[3]))
+    
+    
+#centroiding individual X and Y values from lists
+for i in range(len(arrX)):
+    currx, curry = centroid(arrX[i],arrY[i],radius1, image)
+    arrcentroidX.append(currx)
+    arrcentroidY.append(curry)
+#centroiding asteroid
+astX, astY = centroid(astX,astY, radius2,image)
+
+#performing LSPR
+LSPR2(arrRA, arrDEC, arrcentroidX, arrcentroidY, astX, astY)
+#LSPR2(arrRA, arrDEC, arrX, arrY, 813.4701022025868, 449.373471013335)
+
+
+    
+    
+    
+    
+
---
+++ ODCode/CircularCentroid.py
@@ -0,0 +1,38 @@
+import numpy as np
+from astropy.io import fits
+import math
+im = fits.getdata('Aligned_reduced0000.fits')
+image = np.array(im)
+
+def centroid(x, y, r, m):
+    x = int(float(x))
+    y = int(float(y))
+    length = 2*r+1
+    arrX = []
+    arrY = []
+    meanX = 0
+    meanY = 0
+    
+    for row in range(length):
+        ro = x-r+row
+        for column in range(length):
+            c = y-r+column
+            if ((x-ro)**2 + (y-c)**2) > r**2:
+                m[c][ro] = 0
+    for row in range(length):
+        arrY.append(np.sum(m[row + y -r ,x-r:x+r+1]))
+    for column in range(length):
+        arrX.append(np.sum(m[y-r: y+r+1,column + x - r]))
+    for v in range(len(arrX)):
+        meanX += arrX[v] * (v + x-r)
+    for v in range(len(arrY)):
+        meanY += arrY[v] * (v + y-r)
+    arrX = np.array(arrX)
+    arrY = np.array(arrY)
+    meanX = meanX / np.sum(arrX)
+    meanY = meanY / np.sum(arrY)
+    
+         
+    return meanX, meanY
+ 
+print(centroid(200,200,10,image))
